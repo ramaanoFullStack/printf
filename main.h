c329bb607773c69e7265311f96cc91dad2660080
ramaanoFullStack <rami.stalin.m@outlook.com>
2022-10-17 05:36:08 -0700

    Modified main.h with new prototype

--- main.h
+++ main.h
@@ -20,5 +20,8 @@ int (*f)(va_list);
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_c(va_list array_list);
+int (*get_func(char s))(va_list array_list);
+int print_s(va_list array_list);
 
 #endif
